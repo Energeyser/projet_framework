e05b96fc46620600585511822eb3385f4433a5b3
Pouit <skyrunner106@gmail.com>
2015-12-09 17:30:22 +0100

    aa

--- views/promos.tpl.php
+++ views/promos.tpl.php
@@ -19,6 +19,8 @@
                 ?>
                     <tr>
                         <td><?php echo $donnees['promo']?></td>
+                        <td><button type="button" class="btn btn-lg btn-default">Modifier</button></td>
+                        <a  href="./suppr"<td><button type="button" class="btn btn-lg btn-default">Supprimer</button></td>
                     </tr>
                 <?php
                 }
