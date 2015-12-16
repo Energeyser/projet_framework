a3deb0069f1fb3a935ae1b98851a81e792fd72c3
Pouit <skyrunner106@gmail.com>
2015-12-16 15:40:38 +0100

    lourd

--- views/promos.tpl.php
+++ views/promos.tpl.php
@@ -1,42 +1,45 @@
 <?php content_for('main'); ?>
-    <h1>Promos</h1></br>
-    <?php
-    try {
-        $bdd = new PDO('mysql:host=localhost;dbname=rentree;charset=utf8', 'root', '');
-        $reponse = $bdd->query('SELECT promo FROM `document` GROUP BY promo');
-    ?>
-    <div class="row">
-        <div class="col-md-6">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Promo</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                while($donnees = $reponse->fetch()) {
-                ?>
+    <link href="./css/style.css" rel="stylesheet">
+    <div class="container">
+        <h2>Promos</h2></br>
+        <?php
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=rentree;charset=utf8', 'root', '');
+            $reponse = $bdd->query('SELECT promo FROM `document` GROUP BY promo');
+        ?>
+        <div class="row">
+            <div class="col-md-6">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td><?php echo $donnees['promo']?></td>
-                        <td><button type="button" class="btn btn-lg btn-default">Modifier</button></td>
-                        <td><button type="button" class="btn btn-lg btn-default">Supprimer</button></td>
+                        <th>Promo</th>
                     </tr>
-                <?php
-                }
-                ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php
+                    while($donnees = $reponse->fetch()) {
+                    ?>
+                        <tr>
+                            <td><?php echo $donnees['promo']?></td>
+                            <td><button type="button" class="btn btn-lg btn-default">Modifier</button></td>
+                            <td><button type="button" class="btn btn-lg btn-default">Supprimer</button></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        } catch(Exception $e) {
+            die('Erreur: '.$e->getMessage());
+        }
+        ?>
+        <p>
+            <a href="./ajouter_promo"><button type="button" class="btn btn-lg btn-default">Ajouter</button></a>
+            <button type="button" class="btn btn-lg btn-default">Modifier</button>
+            <button type="button" class="btn btn-lg btn-default">Supprimer</button>
+        </p>
+        <a href="../admin.php"><button type="button" class="btn btn-lg btn-default">Retour</button></a>
     </div>
-    <?php
-    } catch(Exception $e) {
-        die('Erreur: '.$e->getMessage());
-    }
-    ?>
-    <p>
-        <a href="./ajouter_promo"><button type="button" class="btn btn-lg btn-default">Ajouter</button></a>
-        <button type="button" class="btn btn-lg btn-default">Modifier</button>
-        <button type="button" class="btn btn-lg btn-default">Supprimer</button>
-    </p>
-    <a href="../admin.php"><button type="button" class="btn btn-lg btn-default">Retour</button></a>
 <?php end_content_for(); ?>
